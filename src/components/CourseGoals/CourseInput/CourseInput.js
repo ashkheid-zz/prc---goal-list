3e29fb59a5a19b69f348a994aa413e5abfe69691
Ashkan HeidaryFazel <ashkanheidaryfazel@gmail.com>
2022-02-26 09:57:54 +0330

    Conditional css Class

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -23,16 +23,9 @@ const CourseInput = (props) => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label style={{ color: !isValid ? 'red' : 'black' }}>Course Goal</label>
-        <input
-          type="text"
-          onChange={goalInputChangeHandler}
-          style={{
-            borderColor: !isValid ? 'red' : 'black',
-            backgroundColor: !isValid ? 'lightcoral' : 'white',
-          }}
-        />
+      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
